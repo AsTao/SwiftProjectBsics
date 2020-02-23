1ade2df05e7122a467fe0cd86d6690e8a4e43bdf
tao <>
2020-02-23 18:30:49 +0800

    iamge

--- SwiftProjectBsics/Classes/Extentions/UIImageView+Extension.swift
+++ SwiftProjectBsics/Classes/Extentions/UIImageView+Extension.swift
@@ -27,3 +27,24 @@ extension UIImageView{
     }
 
 }
+
+
+extension UIButton{
+    
+    public func setImage(url: String, state: UIControl.State, placeholder :UIImage? = nil , radius :CGFloat = 0){
+        contentMode = .scaleAspectFill
+        setImage(placeholder, for: state)
+        kf.setImage(with: _URL(url), for: state) {
+            [weak self] result in
+            switch result {
+            case .success( _):
+                if radius > 0 {
+                    self?.drawRectWithRoundedCorner(radius: radius)
+                }
+            case .failure(_):
+                self?.setImage(placeholder, for: state)
+            }
+        }
+    }
+
+}
